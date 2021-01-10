84d0a0355c205d2e64e1a2373f26168f8a378ed0
rpmlourenco <rpmlourenco@gmail.com>
2021-01-10 13:27:14 +0100

    Fix executable name and pom variables

--- src/main/java/org/example/SymlinkInstaller.java
+++ src/main/java/org/example/SymlinkInstaller.java
@@ -7,7 +7,7 @@ public class SymlinkInstaller {
     public static void main(String[] args) {
 
         final String dir = System.getProperty("user.dir");
-        System.out.println("current dir = " + dir);
+        System.out.println("The current dir = " + dir);
         System.out.println("Press ENTER to continue...");
         try {
             System.in.read();
