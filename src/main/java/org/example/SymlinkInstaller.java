47403552c3fc2aa44e35175f09cbc8272b325932
rpmlourenco <rpmlourenco@gmail.com>
2021-01-10 12:55:24 +0100

    Initial Commit

---
+++ src/main/java/org/example/SymlinkInstaller.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.IOException;
+
+public class SymlinkInstaller {
+
+    public static void main(String[] args) {
+
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+        System.out.println("Press ENTER to continue...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
